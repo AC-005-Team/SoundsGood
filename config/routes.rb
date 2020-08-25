3654dc397f93a826350f9b53fd42d5f2c95ef3b9
tungchinlee <52619302+tungchinlee@users.noreply.github.com>
2020-08-25 13:12:27 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', omniauthcallbacks: 'users/omniauthcallbacks'}
   
-  resources :songs
+  resources :songs, only: [:create, :index, :new]
 end
