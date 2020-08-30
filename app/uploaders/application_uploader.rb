feb1e731657b4368c7896455659e1bb6dfcf6281
tungchinlee <tungchinli@gmail.com>
2020-08-30 13:54:54 +0800

    1.Add song-delete function & btn   Try to figure it out how to delete file from s3 at the same time after user click btn 2.Add single song page

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -2,4 +2,5 @@ class ApplicationUploader < Shrine
   plugin :validation_helpers
   plugin :remove_invalid
   plugin :pretty_location
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', omniauthcallbacks: 'users/omniauthcallbacks'}
   
-  resources :songs, only: [:create, :index, :new]
+  resources :songs, only: [:create, :index, :new, :destroy, :show]
 
   get '/user/profile', to:'home#profile', as:'user_profile'
   get '/user/:id/edit', to:'home#edit', as:'user_edit'
