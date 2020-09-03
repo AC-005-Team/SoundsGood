a74b74a00a297d69bd60c2c4381b00010e2f9558
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-03 18:42:44 +0800

    remove unused migration

--- db/migrate/20200831091026_remove.rb
+++
@@ -1,5 +0,0 @@
-class Remove < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :follows
-  end
-end
--- db/migrate/20200901081136_drop_track.rb
+++
@@ -1,5 +0,0 @@
-class DropTrack < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :tracks
-  end
-end
