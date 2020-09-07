964d11c6d33a4a4f345ff44a6d8cd610e15bbb56
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-07 18:05:17 +0800

    fix turbolink problem by closing it when linked to

--- app/javascript/scripts/ShareModal.js
+++ app/javascript/scripts/ShareModal.js
@@ -1,27 +1,30 @@
 //於頁面點擊share時，出現蓋板遮罩與share視窗，share視窗會撈取本頁面網址並放置進各大網站的share link中
+// location.reload()
+
+document.addEventListener("DOMContentLoaded", function() {
   const body = document.querySelector('body')
   const shareBtn = document.getElementById('share-song')
   const thePageLink = window.location.href
   var modalClosing
-  
-  shareBtn.addEventListener('click', () => {
-    shareBtn.setAttribute('disabled', 'true')
-    let scroll = window.scrollY
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scroll}px`;
+  if(shareBtn){
+    shareBtn.addEventListener('click', () => {
+      shareBtn.setAttribute('disabled', 'true')
+      let scroll = window.scrollY
+      document.body.style.position = 'fixed'
+      document.body.style.top = `-${scroll}px`
 
-    const shareDiv = document.createElement('div')
-    shareDiv.classList.add('share-container')
-    shareDiv.innerHTML = '<div class="share-fade"><div class="share-mask"></div><button class="share-close-btn"> X </button></div><div class="share-modal"></div>'
+      const shareDiv = document.createElement('div')
+      shareDiv.classList.add('share-container')
+      shareDiv.innerHTML = '<div class="share-fade"><div class="share-mask"></div><button class="share-close-btn"> X </button></div><div class="share-modal"></div>'
 
-    const mask = shareDiv.querySelector('.share-fade')
-    const modal = shareDiv.querySelector('.share-modal')
+      const mask = shareDiv.querySelector('.share-fade')
+      const modal = shareDiv.querySelector('.share-modal')
 
-    modal.innerHTML = `${thePageLink}`
+      modal.innerHTML = `${thePageLink}`
 
-    body.appendChild(shareDiv)
-    setTimeout(() => {modal.classList.toggle('modal-show')}, 300);
-    setTimeout(()=>{mask.classList.toggle('mask-show')},10)
+      body.appendChild(shareDiv)
+      setTimeout(() => {modal.classList.toggle('modal-show')}, 300);
+      setTimeout(()=>{mask.classList.toggle('mask-show')},10)
 
       mask.addEventListener('click', () => {
         if (!modalClosing) {
@@ -40,10 +43,10 @@
           document.body.style.top = ''
           window.scrollTo(0, scroll || 0)
           shareBtn.removeAttribute('disabled')
-          
         } else {
           return
         }
       })
-
-  })
+    })
+  }
+})
