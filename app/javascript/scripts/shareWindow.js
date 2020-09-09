a919c4eb1ceaa3eb9a318873012083d494467222
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-09 19:27:02 +0800

    let popout window positioned center

--- app/javascript/scripts/shareWindow.js
+++ app/javascript/scripts/shareWindow.js
@@ -1,28 +1,46 @@
 export function shareWindow(){
-  var currentSharing = 'http://google.com'
-  var songname = 'name'
-  var songtitle = 'title'
-  var artist = 'artist'
+  const rootPath = window.location.host
   const shareTwitter = document.querySelector('.share-twitter')
   const shareFacebook = document.querySelector('.share-facebook')
 
-  shareTwitter.addEventListener('click', () => {
+  shareTwitter.addEventListener('click', (e) => {
     console.log('clicked')
-    window.open(`https://twitter.com/intent/tweet?url=${currentSharing}&text=Have%20you%20heard%20%E2%80%98${songname}%20-%20${songtitle}%E2%80%99%20by%20${artist}%20on%20%23SoundCloud%3F%20%23np&related=soundcloud`,'','height=400,width=600,top=200,screenX=200,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no')
+    let dataSet = e.target.dataset
+    openWindow(getTwitterUrl(rootPath, dataSet.link, dataSet.name,dataSet.artist),'',600,400)
   })
 
-
-  shareFacebook.addEventListener('click', () => {
+  shareFacebook.addEventListener('click', (e) => {
     console.log('clicked')
-    window.open(getFacebookUrl(),'','height=400,width=600,top=200,screenX=200,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no')
+    let dataSet = e.target.dataset
+    openWindow(getFacebookUrl(rootPath, dataSet.link),'',600,400)
+
   })
-  
-  function getFacebookUrl(){
-    return `https://www.facebook.com/sharer/sharer.php?u=${currentSharing}`
+
+  function openWindow(url, name, width, height){
+    var screenLeft=0, screenTop=0;
+    if(!name) name = ''
+    if(!width) width = 600
+    if(!height) height = 400
+    if(typeof window.screenLeft !== 'undefined') {
+      screenLeft = window.screenLeft
+      screenTop = window.screenTop
+    } else if(typeof window.screenX !== 'undefined') {
+      screenLeft = window.screenX
+      screenTop = window.screenY
+    }       
+    let PosX = screenLeft + (screen.width - width) / 2
+    let PosY = screenTop + (screen.height - height)*0.75 / 2
+    let features = `width=${width},height=${height},top=${PosY},left=${PosX},toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no`
+    var win = window.open(url,name,features)
+    win.focus()
+    return false
+  }
+
+  function getFacebookUrl(root, link){
+    return `https://www.facebook.com/sharer/sharer.php?u=${root}${link}`
   }
   
+  function  getTwitterUrl(root, link, name, artist) {
+    return `https://twitter.com/intent/tweet?url=${root}${link}&text=Have%20you%20heard%20%E2%80%98${name}%E2%80%99%20by%20${artist}%20on%20%23SoundCloud%3F%20%23np&related=soundcloud`
+  }
 }
-
-//https://soundcloud.com/naoyakaorudfsdfsf/w7ck7-norcn#t=0:23
-
-//https://twitter.com/intent/tweet?url=https%3A//soundcloud.com/heldeeprecords/wongo-real-love-feat-mia-milla-out-now&text=Have%20you%20heard%20%E2%80%98Wongo%20-%20Real%20Love%20(feat%20Mia%20Milla)%20%5BOUT%20NOW%5D%E2%80%99%20by%20Heldeep%20Records%20on%20%23SoundCloud%3F%20%23np&related=soundcloud
