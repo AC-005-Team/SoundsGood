78ab326a0d55bec22a866c54a54af45243d1be40
tungchinlee <tungchinli@gmail.com>
2020-09-10 15:27:01 +0800

    user edit page's path fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,12 +17,12 @@ require('stylesheets')
 //
 const images = require.context('../images', false)
 const imagePath = (name) => images(name, false)
-
+import './preview'
 import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
 // require('./style.css')
-import './preview'
+
 
 
 
