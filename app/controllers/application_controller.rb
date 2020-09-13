d3467db9b7d80befaba622175cbfdbd2bab95323
Leo Li <boypie510@gmail.com>
2020-09-13 14:04:38 +0800

    set the link path for songs/index.html and show.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  private
+  def after_sign_in_path_for(resource)
+    discover_path
+  end
+  
+  def after_sign_up_path_for(resource)
+    discover_path
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -55,10 +55,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     discover_path
   end
 
-  def after_sign_in_path_for(resource)
-    discover_path
-  end
-
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
