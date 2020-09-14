a6de81ac49b443884db10d6ac98c6755e30e77ab
tungchinlee <tungchinli@gmail.com>
2020-09-14 11:57:18 +0800

    1.add tag feature with select2 add tag model & sons_tag model add select2 add tag-create-feature logic in song model add select2's js in tag.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("styles")
 // require("functions.js")
 require('stylesheets')
 
+
 const images = require.context('../images', false)
 const imagePath = (name) => images(name, false)
 import './preview'
@@ -23,8 +24,10 @@ import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
 // require('./style.css')
-
-
+// import "bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
+import "select2/dist/css/select2.css"
+import "./tag"
 
 
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -8,4 +8,14 @@ class Song < ApplicationRecord
   has_many :comments
   has_many :songs_tags
   has_many :tags, through: :songs_tags
+
+  def tag_items
+    tags.map(&:name)
+  end
+  
+  def tag_items=(names)
+    self.tags = names.map{|item|
+      Tag.where(name: item.strip).first_or_create! unless item.blank?}.compact
+  end
+
 end
