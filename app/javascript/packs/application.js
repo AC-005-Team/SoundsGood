b40b46b0d685c0424f5c8befd30e3d824b2175ef
steven <steven@tenlong.com.tw>
2020-09-18 14:18:06 +0800

    user/1/songs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ import "tailwindcss/components";
 import "tailwindcss/utilities";
 import "@fortawesome/fontawesome-free/js/all";
 // require('./style.css')
-import "bootstrap/dist/css/bootstrap.css"
+// import "bootstrap/dist/css/bootstrap.css"
 import "select2/dist/css/select2.css"
 import "./tag"
 
