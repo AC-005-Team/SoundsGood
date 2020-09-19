1c79d7ed7d6df36ba1d8cfaaa7aa280d1d263912
tungchinlee <tungchinli@gmail.com>
2020-09-19 17:29:45 +0800

    1.search feature sec edition  add search routes  add search controller & result.html.erb page

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,26 @@
+class SearchesController < ApplicationController
+
+  def show
+    return if params[:search].blank?
+    
+    @content = params[:search].downcase
+    @users = User.name_search("display_name", @content).map(&:display_name)
+    @songs = Song.name_search("name", @content).map(&:name)
+    @Playlists = Playlist.name_search("name", @content).map(&:name)
+    @result = @users | @songs | @Playlists
+    respond_to do |format|
+      format.html
+      format.json { render json: @result }
+    end
+  end
+
+  def result
+    return if params[:search].blank? 
+
+    @name = params[:search].downcase
+    @users = User.name_search("display_name", @name)
+    @songs = Song.name_search("name", @name).includes(:tags)
+    @Playlists = Playlist.name_search("name", @name)
+  end
+
+end
--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,10 +1,36 @@
-import Rails from '@rails/ujs'
-
 document.addEventListener('turbolinks:load', function (){
-  document.querySelector('#search').addEventListener('keyup', search)
+  document.querySelector('#search').addEventListener('keyup', function(e){
+    if( e.key === "Enter" ){
+      result()
+    }else{
+      document.querySelector('.search-result').innerHTML = ""
+      search()
+    }
+  })
 })
 
 function search(){
-  let search_context = document.querySelector('#search_bar')
-  Rails.fire(search_context, 'submit')
+  const search = document.querySelector('#search')
+  const search_value = search.value
+  console.log(search_value)
+  if( search_value === "" ){ return } 
+  const search_recommend  = document.querySelector('.search-result')
+  fetch(`http://127.0.0.1:3000/search.json?search=${search_value}`)
+    .then(response => response.json())
+    .then(result => result.forEach(
+      element => {
+        const class_list = ["px-2", "m-2", "bg-gray-200", "rounded", "w-12/12"]
+        const a_tag = document.createElement('a')
+              a_tag.classList.add(...class_list)
+              a_tag.href = 'result_search_path'
+              a_tag.textContent = `${element}`
+        console.log(element)
+        search_recommend.appendChild(a_tag)
+      }
+    ))
+}
+
+function result(){
+  const search_result = document.querySelector('#search_bar')
+  search_result.submit()
 }
