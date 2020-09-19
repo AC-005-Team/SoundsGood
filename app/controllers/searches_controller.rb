95ee152515ff19bd1126fe9340aebb06dbe3a1a5
tungchinlee <tungchinli@gmail.com>
2020-09-19 21:52:43 +0800

    useless code deleted

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,14 +9,13 @@ class SearchesController < ApplicationController
     @Playlists = Playlist.name_search("name", @content).map(&:name)
     @result = @users | @songs | @Playlists
     respond_to do |format|
-      format.html
       format.json { render json: @result }
     end
   end
 
   def result
     return if params[:search].blank? 
-    puts params
+
     @name = params[:search].downcase
     @users = User.name_search("display_name", @name)
     @songs = Song.name_search("name", @name).includes(:tags)
