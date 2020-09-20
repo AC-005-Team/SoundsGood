c419ab90c971bf5e63e7d9b045971d939be5df33
Leo Li <boypie510@gmail.com>
2020-09-20 20:12:56 +0800

    integrate comment function into tailwindcss format

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,12 +1,3 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus";
 import Rails from "@rails/ujs";
 
@@ -14,7 +5,6 @@ export default class extends Controller {
   static targets = ["heart", "likeBtn", "repostBtn"];
   like() {
     let song_id = this.data.get("song");
-    // console.log(this.data.get("song"));
     Rails.ajax({
       url: `/songs/${song_id}/like.json`,
       type: "post",
@@ -47,14 +37,12 @@ export default class extends Controller {
 
   repost() {
     let song_id = this.data.get("song");
-    // console.log(this.data.get("song"));
     Rails.ajax({
       url: `/songs/${song_id}/repost.json`,
       type: "post",
       success: (result) => {
         let btnContent = this.repostBtnTarget.lastElementChild;
         if (result["status"] == true) {
-          //愛心變實心
           this.repostBtnTarget.classList.remove("border-grey-400");
           this.repostBtnTarget.classList.add(
             "text-red-600",
@@ -62,7 +50,6 @@ export default class extends Controller {
           );
           btnContent.textContent = "Reposted";
         } else {
-          //變空心
           this.repostBtnTarget.classList.remove(
             "text-red-600",
             "border-orange-500"
@@ -76,7 +63,4 @@ export default class extends Controller {
       },
     });
   }
-  // connect() {
-  //   this.outputTarget.textContent = 'Hello, Stimulus!'
-  // }
 }
