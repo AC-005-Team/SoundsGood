058b489a98ba51137fd09ea2537bbd43903f0da9
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-21 18:11:22 +0800

    serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
 end
