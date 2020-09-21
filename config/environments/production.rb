e84386676f27e9c39433a40c79cddaa8cf0718aa
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-21 18:07:46 +0800

    serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.serve_static_assets = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
