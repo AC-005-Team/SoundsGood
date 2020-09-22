66e5123fd1008ee618cb3cb75bfec94719e86ece
PoYi Li <37627151+boypie510@users.noreply.github.com>
2020-09-22 11:16:35 +0800

    Feature/stimulus (#27)
    * install stimulus & set ajax for like action of song
    
    * Add the ajax for like and repost function by Railsajax
    
    * integrate comment function into tailwindcss format
    
    * 1. set the comment reply_id default from 0 to nil
    2. Add the create.js.erb for comment function instead of redirect to same page
    
    * use ajax instead of redirect on add_to_playlist function
    
    * use create.js.erb for comment create to replace rediret
    
    * integrate soundwave in songs/show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,39 +3,35 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("scripts")
-require("styles")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("scripts");
+require("styles");
 
 // require("aplayer")
 
 // require("fron-end")
 // require("functions.js")
+require("stylesheets");
 
-require('stylesheets')
-require('scripts')
-
-
-const images = require.context('../images', false)
-const imagePath = (name) => images(name, false)
-import './preview'
+const images = require.context("../images", false);
+const imagePath = (name) => images(name, false);
+import "./preview";
 import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
 import "@fortawesome/fontawesome-free/js/all";
-import WaveSurfer from 'wavesurfer.js'
+import WaveSurfer from "wavesurfer.js";
 // require('./style.css')
 // import "bootstrap/dist/css/bootstrap.css"
-import "select2/dist/css/select2.css"
-import "./tag"
-import "./search"
-
-
-
+import "select2/dist/css/select2.css";
+import "./tag";
+import "./search";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
+
+import "controllers";
