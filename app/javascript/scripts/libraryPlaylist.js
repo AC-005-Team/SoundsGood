9d4e3d73619d7998e6129a9ba2b9da549ca404c6
Kelsey.Chen <naoyakaoru@gmail.com>
2020-09-30 12:59:06 +0800

    fix playlist img_url to get first song in list

--- app/javascript/scripts/libraryPlaylist.js
+++ app/javascript/scripts/libraryPlaylist.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const parentDom = document.querySelector('.library-playlist-place')
   const hostPath = 'http://'+window.location.host
   const template = document.querySelector('template')
   if(parentDom){
+    parentDom.innerHTML = ''
     fetch(`${hostPath}/api/v1/playlists/library`)
     .then( res=> {
       if(!res.ok){
@@ -11,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
       return res.json()
     })
     .then( playlists => {
-      playlists.forEach(playlist => {
-        appendEl(playlists)
+      playlists.forEach( playlist => {
+        appendEl(playlist)
       })
     })
     .catch( err=> {
@@ -21,12 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function appendEl(playlist){
       const domEl = document.createElement('div')
-      domEl.innerHTML = template.textContent
+      domEl.innerHTML = template.innerHTML
       const imgPlace = domEl.querySelector('.set-img-place')
       const infoPlace = domEl.querySelector('.set-info-place')
       const playlistPath = hostPath+`/playlists/${playlist.playlist_id}`
       const userPath = hostPath+`/users/${playlist.user_id}`
-      imgPlace.innerHTML = `<a class="block"><img src="${playlist.img}"></a>`
+      imgPlace.innerHTML = `<a class="block"><img src=${playlist.img_url}></a>`
       infoPlace.innerHTML = `<li><a href="${playlistPath}">${playlist.name}</a></li><li>${playlist.user_name}</li>`
       parentDom.appendChild(domEl)
     }
--- app/views/api/v1/playlists/library.json.jbuilder
+++ app/views/api/v1/playlists/library.json.jbuilder
@@ -1,5 +1,6 @@
 json.array! @playlists do |playlist|
   json.name playlist.name
+  json.img_url playlist.songs.first.image_url
   json.user_id playlist.user_id
   json.user_name playlist.user.display_name
   json.playlist_id playlist.id
