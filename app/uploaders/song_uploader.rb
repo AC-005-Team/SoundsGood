3b5240312094de2bb41d0333e16039e9f2b6de4a
Leo Li <boypie510@gmail.com>
2020-10-04 19:03:54 +0800

    fix the bug on songs/index

--- app/uploaders/song_uploader.rb
+++ app/uploaders/song_uploader.rb
@@ -1,11 +1,11 @@
 class SongUploader < ApplicationUploader
   plugin :add_metadata
 
-  add_metadata do |io, context|
-    audio = Shrine.with_file(io) { |file| FFMPEG::Movie.new(file.path) }
-    { "duration"   => audio.duration
-    }
-  end
+  # add_metadata do |io, context|
+  #   audio = Shrine.with_file(io) { |file| FFMPEG::Movie.new(file.path) }
+  #   { "duration"   => audio.duration
+  #   }
+  # end
   Attacher.validate do
     validate_mime_type %w[audio/mp3 audio/ogg audio/mpeg audio/m4a], message: "must be audio/mp3 audio/ogg audio/mpeg audio/m4a"
   end
