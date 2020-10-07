8411a4624d936a4d99264135ccc4626cad218d6f
Leo Li <boypie510@gmail.com>
2020-10-07 17:43:19 +0800

    fix the playlist toggle function

---
+++ app/javascript/scripts/addPlaylist.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector("#addplaylist-modal")
+    .lastElementChild.addEventListener("click", () => {
+      console.log("hi");
+    });
+});
--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -1,4 +1,5 @@
-import './modal'
-import './wave'
-import './comment'
-import './libraryPlaylist'
+import "./modal";
+import "./wave";
+import "./comment";
+import "./libraryPlaylist";
+import "./addPlaylist";
