3f2814cda297515777d92df392eff79d08c6a83c
Leo Li <boypie510@gmail.com>
2020-10-15 21:31:51 +0800

    comment bug fix

--- app/javascript/scripts/showReplyForm.js
+++ app/javascript/scripts/showReplyForm.js
@@ -2,12 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll(".replyBtn").forEach((replyBtn) => {
     replyBtn.addEventListener("click", (e) => {
       const commentId = e.currentTarget.dataset.id;
-      let replyForms = document.querySelectorAll(".replyForm");
       let replyForm = document.querySelector(`.comment-id-${commentId}`)
         .lastElementChild;
-      replyForms.forEach((form) => {
-        form.classList.add("hidden");
-      });
       replyForm.classList.remove("hidden");
     });
   });
