e70339fe33a390bb90367e0674da71bf141fe457
lee Helen <dzzzzzb@gmail.com>
2020-10-16 12:44:50 +0800

    1016

--- app/javascript/components/page/you/you.vue
+++ app/javascript/components/page/you/you.vue
@@ -3,13 +3,13 @@
     <div class="flex text-xl md:text-2xl mx-4 text-white">
       <button class="mr-4 tab tab-underline"><router-link :to="{name: 'library'}">Overview</router-link></button>
       <button class="mr-4 tab"><router-link :to="{ name: 'likes'}">Likes</router-link></button>
-      <button class="mr-4 tab">  <router-link :to="{ name: 'playlists' }">playlists</router-link></button>
+      <button class="mr-4 tab">  <router-link :to="{ name: 'playlists' }">Playlists</router-link></button>
       <!-- <button class="mr-4 hover:text-orange-500"><a>Albums</a></button> -->
       <!-- <button class="mr-4 hover:text-orange-500"><a>Stations</a></button> -->
-      <button class="mr-4"><router-link :to="{ name: 'following' }">following</router-link></button>
+      <button class="mr-4"><router-link :to="{ name: 'following' }">Following</router-link></button>
       <!-- <button class="mr-4 hover:text-orange-500"><a>History</a></button> -->
     </div>
-  
+
     <router-view> </router-view>
   </div>
 
