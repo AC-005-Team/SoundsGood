1acde4b2835451aeda75c1435d24adddf776027c
Leo Li <boypie510@gmail.com>
2020-10-16 22:30:42 +0800

    WIP

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::SongsController < ApplicationController
   def index
     @songs = Song.all.order(id: :desc)
     @playlists = current_user.playlists
-    @tag1_songs = Tag.find_by(name: "coding").songs
+    if Tag.find_by(name: "coding")
+      @tag1_songs = Tag.find_by(name: "coding").songs
+    else
+      @tag1_songs = []
+    end 
   end
 
   def show
@@ -22,5 +26,10 @@ class Api::V1::SongsController < ApplicationController
       # format.html {redirect_to @song}
       format.json {render json: {status: @song.added_by?(@playlist)}}
     end
+  end
+
+  def test
+    @songs = Song.all
+    @song_top5 = @songs.select {|x| x.liked_users.size}.max(5)
   end
 end
