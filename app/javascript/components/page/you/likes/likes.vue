9ab7cfac91f9a77a11d18ae587b97edfded1d853
Leo Li <boypie510@gmail.com>
2020-10-17 23:30:51 +0800

    fix some css

--- app/javascript/components/page/you/likes/likes.vue
+++ app/javascript/components/page/you/likes/likes.vue
@@ -1,79 +1,86 @@
-<template >
+<template>
   <div class="col-span-9 m-4">
-    <span class="text-sm text-white bg-leego_orange border border-leego_orange px-2">review the song you like</span>
+    <span
+      class="text-sm text-white bg-leego_orange border border-leego_orange px-2"
+      >Review the song you like</span
+    >
     <div class="flex justify-between">
-      <div class="grid sm:grid-cols-2 md:col-span-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-10">
+      <div
+        class="grid sm:grid-cols-2 md:col-span-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-10"
+      >
         <div class="wrapper">
-          <div class="card text-center flex items-center justify-center" @click="playLikelist">
-            <i class="fas z-10 fa-play" style="font-size: 40px"/>
+          <div
+            class="card text-center flex items-center justify-center"
+            @click="playLikelist"
+          >
+            <i class="fas z-10 fa-play" style="font-size: 40px" />
             <p style="font-size: 20px">ALL</p>
           </div>
- <!-- <img class="object-cover card" src="/img/night.jpg" @click="playLikelist"> -->
-
+          <!-- <img class="object-cover card" src="/img/night.jpg" @click="playLikelist"> -->
         </div>
-        <likeSongs v-for="song in songs" :song="song" :songs="songs"/>
+        <likeSongs v-for="song in songs" :song="song" :songs="songs" />
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-import likeSongs from './like_songs'
+import likeSongs from "./like_songs";
 import Api from "../../../../api/api";
 
-import {
-  mapState,
-  mapGetters,
-  mapActions
-} from 'vuex'
-
-
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
-  data(){
-    return{
-      songs:null,
-    }
+  data() {
+    return {
+      songs: null,
+    };
   },
   components: {
-    likeSongs
-    },
-  computed:{
-      ...mapGetters({
-        like_songs: 'song/like_songs',
-        isPLAY: 'songs/isPLAY',
-        playerTracks:'songs/playerTracks',
+    likeSongs,
+  },
+  computed: {
+    ...mapGetters({
+      like_songs: "song/like_songs",
+      isPLAY: "songs/isPLAY",
+      playerTracks: "songs/playerTracks",
+    }),
+  },
+  ...mapActions("songs", [
+    "play",
+    "pause",
+    "continuePlay",
+    "continuePause",
+    "setPlayerTracks",
+  ]),
+  methods: {
+    playLikelist() {
+      var likeTag = this.songs.map((a) => a.audio);
 
-      }),
-
-    },
-    ...mapActions("songs", ["play", "pause", "continuePlay", "continuePause",'setPlayerTracks']),
-    methods:{
-      playLikelist(){
-        var likeTag = this.songs.map( a => a.audio );
-
-        if( this.playerTracks[0] && (likeTag[0].artist === this.playerTracks[0].artist)){
-          if(this.isPLAY == true){
-            this.$store.dispatch('songs/pause')
-          }else{
-            this.$store.dispatch('songs/play')
-          }
-        }else{
-          this.$store.dispatch('songs/setPlayerTracks', likeTag)
-          this.$store.dispatch('songs/play')
+      if (
+        this.playerTracks[0] &&
+        likeTag[0].artist === this.playerTracks[0].artist
+      ) {
+        if (this.isPLAY == true) {
+          this.$store.dispatch("songs/pause");
+        } else {
+          this.$store.dispatch("songs/play");
         }
-
+      } else {
+        this.$store.dispatch("songs/setPlayerTracks", likeTag);
+        this.$store.dispatch("songs/play");
       }
     },
-//為什麼會失敗！！！！！！
-// ...mapActions('song', ['song/loadlikeSongs']),
-created(){
-  Api().get('/api/v1/library/likes')
-      .then(response => (this.songs = response.data))
-  // this.$store.dispatch('song/loadlikesongs')
-  }
-}
+  },
+  //為什麼會失敗！！！！！！
+  // ...mapActions('song', ['song/loadlikeSongs']),
+  created() {
+    Api()
+      .get("/api/v1/library/likes")
+      .then((response) => (this.songs = response.data));
+    // this.$store.dispatch('song/loadlikesongs')
+  },
+};
 </script>
 
 <style lang="css" scoped>
@@ -87,9 +94,8 @@ created(){
   border-radius: 10px;
   position: relative;
   transition: transform 0.5s ease-in-out;
-  color:rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
 }
-
 
 .wrapper:hover,
 .card:hover,
@@ -98,6 +104,4 @@ created(){
   transform: none;
   box-shadow: none;
 }
-
-
 </style>
--- app/javascript/components/page/you/playlists/playlists.vue
+++ app/javascript/components/page/you/playlists/playlists.vue
@@ -2,7 +2,7 @@
 <div>
 
   <div class="bg-gray-900 text-white text-base m-4 border-b border-gray-600">
-    <span class="text-sm bg-leego_orange border border-leego_orange px-2">checkout the playlists you create</span>
+    <span class="text-sm bg-leego_orange border border-leego_orange px-2">Checkout the playlists you create</span>
     <div class="flex overflow-x-scroll gap-6">
       <playlistItem v-for="playlist in playlists" :playlist="playlist" ></playlistItem>
     </div>
